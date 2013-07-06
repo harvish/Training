0d3960cc06b1e788cc05163b03992c07d6618a2f
harvish <harvish@gmail.com>
2013-07-06 23:06:12 +0530

    SerilaizationDemo...

---
+++ Training/src/SerializationDemo.java
@@ -0,0 +1,21 @@
+class MySample	implements Serializable{
+	int b;
+	char c;
+	String d;
+	MySample(int b1,char c1,String d1)
+	{
+		b=b1;c=c1;d=d1;
+	}
+	void display()
+	{
+		System.
+	}
+}
+public class SerializationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
